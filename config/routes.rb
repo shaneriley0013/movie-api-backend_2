bb9b89448a3826680ebf3bbacea6495af3f14802
SHANE RILEY <shaneriley0013@gmail.com>
2023-06-10 18:10:52 -0500

    add root, nonworkin' "add fave" button to mov indx

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-   # Defines the root path route ("/")
-  # root "articles#index"
+
+  root "movies#index"
 
   resources :movies
   post '/movies/new', to: 'movies#create'
